7484b9a152c52faa7c75806fae1a6dd212a0b1c5
Gabriel Cacharo <57328274+Gabriel-Cacharo@users.noreply.github.com>
2023-12-05 18:58:59 -0300

    feat: :sparkles: Auth system backend Admin Dashboard

--- backend/src/middleware/Auth.js
+++ backend/src/middleware/Auth.js
@@ -19,4 +19,27 @@ module.exports = {
       return res.status(NOT_AUTHORIZED_CODE).json({ error: NOT_AUTHORIZED_MESSAGE });
     }
   },
+
+  async authAdminMiddleware(req, res, next) {
+    const authHeader = await req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+      return res.status(NOT_AUTHORIZED_CODE).json({ error: NOT_AUTHORIZED_MESSAGE });
+    }
+
+    try {
+      jwt.verify(token, process.env.JWT_SECRET);
+
+      const userTokenInformations = jwt.decode(token);
+
+      if (!userTokenInformations.admin) {
+        return res.status(NOT_AUTHORIZED_CODE).json({ error: NOT_AUTHORIZED_MESSAGE });
+      }
+
+      next();
+    } catch (err) {
+      return res.status(NOT_AUTHORIZED_CODE).json({ error: NOT_AUTHORIZED_MESSAGE });
+    }
+  },
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,31 +1,31 @@
 const router = require('express').Router();
 
-const { authMiddleware } = require('./middleware/Auth');
+const { authMiddleware, authAdminMiddleware } = require('./middleware/Auth');
 
 const User = require('./api/User/userApi');
 const Crate = require('./api/Crate/crateApi');
 const Ripo = require('./api/Ripo/ripoApi');
 
-router.post('/auth/register', User.register);
-router.post('/auth/login', User.login);
-router.post('/auth/forgotPassword', User.forgotPassword);
-router.post('/auth/acceptForgotPassword', User.acceptForgotPassword);
-
+// Admin Routes
 router.post('/auth/admin/login', User.loginAdmin);
 
-router.get('/admin/crates/all', Crate.getAllCrates);
-router.post('/admin/crates/add', Crate.addUserCrate);
-router.put('/admin/crates/editCrateBasicInformations', Crate.editCrateBasicInformations);
-router.put('/admin/crates/editCrateDrops', Crate.editCrateDrops);
+router.get('/admin/crates/all', authAdminMiddleware, Crate.getAllCrates);
+router.post('/admin/crates/add', authAdminMiddleware, Crate.addUserCrate);
+router.put('/admin/crates/editCrateBasicInformations', authAdminMiddleware, Crate.editCrateBasicInformations);
+router.put('/admin/crates/editCrateDrops', authAdminMiddleware, Crate.editCrateDrops);
 
-router.get('/crates/getUserCrates', authMiddleware, Crate.getUserCrates);
-router.post('/crates/redeem', authMiddleware, Crate.redeemCrate);
-router.post('/crates/buy', authMiddleware, Crate.buyCrate);
-router.post('/crates/open', authMiddleware, Crate.openCrate);
+router.get('/admin/user/searchProfile', authAdminMiddleware, User.searchProfileWithCrates);
+router.post('/admin/user/editUserBasicInformations', authAdminMiddleware, User.editUserBasicInformations);
 
 router.get('/admin/crates/getSpecificUserCrates', Crate.getSpecificUserCrates);
 
 router.put('/admin/ripos/editRipoBasicInformations', authMiddleware, Ripo.editRipoBasicInformations);
+
+// User Routes
+router.post('/auth/register', User.register);
+router.post('/auth/login', User.login);
+router.post('/auth/forgotPassword', User.forgotPassword);
+router.post('/auth/acceptForgotPassword', User.acceptForgotPassword);
 
 router.get('/ripos/getUserRipos', authMiddleware, Ripo.getUserRipos);
 router.post('/ripos/sell', authMiddleware, Ripo.sellRipo);
@@ -35,11 +35,14 @@ router.get('/ripos/owner', authMiddleware, User.getPublicRipoOwner);
 router.get('/ripos/all', Ripo.getAllRipos);
 router.post('/ripos/addUserRipos', Ripo.addUserRipos);
 
+router.get('/crates/getUserCrates', authMiddleware, Crate.getUserCrates);
+router.post('/crates/redeem', authMiddleware, Crate.redeemCrate);
+router.post('/crates/buy', authMiddleware, Crate.buyCrate);
+router.post('/crates/open', authMiddleware, Crate.openCrate);
+
 router.get('/users/all', User.getAllUsers);
 router.post('/users/removeRipo', Ripo.removeUserRipo);
 router.post('/users/removeCrate', Crate.removeUserCrate);
-
-router.post('/admin/user/editUserBasicInformations', authMiddleware, User.editUserBasicInformations);
 
 router.get('/user/searchProfile', authMiddleware, User.searchProfile);
 router.get('/user/profile', authMiddleware, User.profile);
@@ -47,7 +50,5 @@ router.post('/user/updateFacRipos', authMiddleware, User.updateUserFacRipos);
 router.post('/user/resetPassword', authMiddleware, User.resetUserPassword);
 router.post('/user/verifyAccountEmail', authMiddleware, User.verifyAccountEmail);
 router.get('/user/searchProfile/autocomplete', authMiddleware, User.searchProfileResponses);
-
-router.get('/admin/user/searchProfile', authMiddleware, User.searchProfileWithCrates);
 
 module.exports = router;
